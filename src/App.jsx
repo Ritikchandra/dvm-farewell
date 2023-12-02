1d8a3e9de8242b03d20de59056537a0fcee0f4a0
Ritikchandra <ritik.chandra009@gmail.com>
2023-12-02 17:13:50 +0530

    carousel implemented

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,34 @@ import './App.css'
 import Logo from "../src/assets/dvmFarewellLogo.png"
 import Arrow from "../src/assets/leftArrow.png"
 import Sunpreet from "../src/assets/sunpreet.png"
+import Ritik from "../src/assets/ritik.jpg"
+import React, {useState} from 'react'
 function App() {
+  const data = [
+    { name: 'Sunpreet', team: 'Design', image: Sunpreet },
+    {name: "Ritik", team: "Frontend", image:Ritik},
+    { name: 'Sunpreet', team: 'Design', image: Sunpreet },
+    {name: "Ritik", team: "Frontend", image:Ritik},
+    { name: 'Sunpreet', team: 'Design', image: Sunpreet },
+    {name: "Ritik", team: "Frontend", image:Ritik},
+    { name: 'Sunpreet', team: 'Design', image: Sunpreet },
+    {name: "Ritik", team: "Frontend", image:Ritik},
+    { name: 'Sunpreet', team: 'Design', image: Sunpreet },
+    {name: "Ritik", team: "Frontend", image:Ritik},
+    { name: 'Sunpreet', team: 'Design', image: Sunpreet },
+    {name: "Ritik", team: "Frontend", image:Ritik},
+  ];
 
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNavigation = (direction) => {
+    const newIndex =
+      direction === 'left'
+        ? (currentIndex - 1 + data.length) % data.length
+        : (currentIndex + 1) % data.length;
+
+    setCurrentIndex(currentIndex==data.length-1? 0:currentIndex+1);
+  };
   return (
     <>
     <div className='pageWrapper'>
@@ -26,16 +52,37 @@ function App() {
         </div>
         <div className="rightSideWrapper">
           <div className="carouselContainer">
-            <div className="leftBtn">
+            <div className="leftBtn" onClick={() => handleNavigation('left')}>
               <img src={Arrow} alt="" />
             </div>
-            <div className="info">
+            {/* <div className="info">
               <img src={Sunpreet} alt="" />
               <div className="infoText">
                 <span className='name'>Sunpreet</span>
                 <span className="team">Design</span>
               </div>
+              <img src={data[currentIndex].image} alt={data[currentIndex].name} />
+          <div className="infoText">
+            <span className="name">{data[currentIndex].name}</span>
+            <span className="team">{data[currentIndex].team}</span>
+          </div>
+            </div> */}
+             <div className="info">
+          {data.map((item, index) => (
+            <div
+              key={index}
+              className={`carouselItem ${
+                index === currentIndex ? 'active' : 'inactive'
+              } ${index === currentIndex - 1 ? 'previous' : ''}`}
+            >
+              <img src={item.image} alt={item.name} />
+              <div className="infoText">
+                <span className="name">{item.name}</span>
+                <span className="team">{item.team}</span>
+              </div>
             </div>
+          ))}
+        </div>
           </div>
         </div>
     <div className="dvm">
