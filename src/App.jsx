c3f9d3b3b465659bb63a61095c1f307fe3bfea48
Ritikchandra <ritik.chandra009@gmail.com>
2023-12-04 19:09:08 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,21 @@ import Logo from "../src/assets/dvmFarewellLogo.png"
 import Arrow from "../src/assets/leftArrow.png"
 import Sunpreet from "../src/assets/sunpreet.png"
 import Ritik from "../src/assets/ritik.jpg"
+import Sarthak from "../src/assets/sarthak.jpeg"
+import Devashish from "../src/assets/vi.jpeg"
+import Praneet from "../src/assets/praneet.jpeg"
 import React, {useState} from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectFlip, Pagination, Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/effect-flip';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 function App() {
   const data = [
-    { name: 'Sunpreet', team: 'Design', image: Sunpreet },
-    {name: "Ritik", team: "Frontend", image:Ritik},
-    { name: 'Sunpreet', team: 'Design', image: Sunpreet },
-    {name: "Ritik", team: "Frontend", image:Ritik},
-    { name: 'Sunpreet', team: 'Design', image: Sunpreet },
-    {name: "Ritik", team: "Frontend", image:Ritik},
-    { name: 'Sunpreet', team: 'Design', image: Sunpreet },
-    {name: "Ritik", team: "Frontend", image:Ritik},
-    { name: 'Sunpreet', team: 'Design', image: Sunpreet },
-    {name: "Ritik", team: "Frontend", image:Ritik},
-    { name: 'Sunpreet', team: 'Design', image: Sunpreet },
-    {name: "Ritik", team: "Frontend", image:Ritik},
+    { name: 'Sarthak ', team: 'Frontend', image: Sarthak },
+    {name: "Praneet", team: "AppDev", image:Praneet},
+    { name: 'Devashish', team: 'AppDev', image: Devashish },
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -89,6 +89,44 @@ function App() {
       <span>DEPARTMENT OF VISUAL MEDIA 2023</span>
     </div>
     </div>
+    <div className="mobilePageWrapper">
+    <div className="mobileLeftSideWrapper">
+        <div className="mobileTopTextWrapper">
+          <div className='mobileFlexContainer'>
+        <div className="heading">
+          Farewell
+        </div>
+          <span className='rotateText'>2020</span>
+          </div></div></div>
+          <div className='mobileSwiperWrapper'>
+          <div className="mobileSwiper">
+          <Swiper
+    effect={'flip'}
+    grabCursor={false}
+    pagination={false}
+    navigation={true}
+    modules={[EffectFlip, Pagination, Navigation]}
+    className="mySwiper"
+  >
+    {data.map((item, index) => (
+      <SwiperSlide key={index}>
+        <div className="swiperContent">
+          <img src={item.image} alt={item.name} />
+          <div className="infoText">
+            <span className="name">{item.name}</span>
+            <span className="team">{item.team}</span>
+          </div>
+        </div>
+      </SwiperSlide>
+    ))}
+  </Swiper>
+          </div>
+          </div>
+    <div className="mobileDvmLogo">
+        <img src={Logo} alt="" />
+      </div>
+    </div>
+
     </>
   )
 }
